d3bf91a7044e8fc6c16a2665e7e4d0becccd9be5
JonasLudwig1998 <j.ludwig@leuchtfeuer.com>
2026-07-14 14:10:58 +0200

    MTC-11112 Fixcs

--- Controller/PublicController.php
+++ Controller/PublicController.php
@@ -106,7 +106,7 @@ class PublicController extends AbstractFormController
         $cookieId       = $request->cookies->get('mtc_id');
         $leadFromCookie = null !== $cookieId ? $leadModel->getEntity($cookieId) : null;
 
-        if (!$leadFromCookie instanceof \Mautic\LeadBundle\Entity\Lead) {
+        if (!$leadFromCookie instanceof Lead) {
             return $this->processWithoutCookie($leadFromQuery, $query, $featureSettings, $leadRepository);
         }
 
